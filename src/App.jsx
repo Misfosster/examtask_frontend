c25f5731d5b435f08f80ec1aeb9b46478992363c
Misfosster <cph-wn11@cphbusiness.dk>
2023-06-13 21:43:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import React, {useState} from "react"
+import facade from "./apiFacade";
+import LogIn from "./components/LoginForm";
+import LoggedIn from "./components/LoggedIn";
+import {NavLink, Route, Routes} from "react-router-dom";
+
+
+function App() {
+    const [loggedIn, setLoggedIn] = useState(localStorage.getItem('setLoggedIn') || false);
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')) || {username: '', roles: ''});
+
+
+    const logout = () => {
+        facade.logout();
+        setUser({username: "", roles: ""});
+        setLoggedIn(false);
+        localStorage.removeItem('setLoggedIn');
+        localStorage.removeItem('user');
+        window.location.replace('/examtask/');
+
+    };
+
+    const login = (user, pass) => {
+        facade.login(user, pass).then(() => {
+            const token = facade.readJwtToken(facade.getToken());
+            setUser({username: token.username, roles: token.roles});
+            setLoggedIn(true);
+            localStorage.setItem('setLoggedIn', true);
+            localStorage.setItem('user', JSON.stringify({username: token.username, roles: token.roles}));
+        });
+    }
+
+    const Header = () => {
+        const handleLogout = () => {
+            logout();
+        };
+
+        return (
+            <div>
+                <ul className="header">
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    {loggedIn && (
+                        <>
+                            
+                            <li>
+                                <NavLink to="/profilepage">Profile</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/logout" onClick={handleLogout}>
+                                    Logout
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+                </ul>
+                <br/>
+            </div>
+        );
+    };
+    
+
+    const Home = () => {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 offset-md-2">
+                        <h2>Home</h2>
+                        {!loggedIn ? (
+                            <LogIn login={login}/>
+                        ) : (
+                            <div>
+                                <h3>Here is your homepage</h3>
+                                
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const Logout = () => {
+        return (
+            <div>
+                <h2>Logout</h2>
+                <div>
+                    <LoggedIn LoggedIn user={user} logout={logout} loggedIn={loggedIn}/>
+                    <button onClick={logout}>Logout</button>x
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <Header/>
+            <Routes>
+                <Route exact path="/" element={<Home/>}></Route>
+                
+                <Route path="/profilepage"
+                       element={<LoggedIn user={user} logout={logout} loggedIn={loggedIn}/>}></Route>
+            </Routes>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Setting.js
@@ -0,0 +1,5 @@
+export const
+    URL = "https://localhost:8080/tomcat/examtask/",
+    dummyURL = "http://localhost:8080/tomcat/examtask/api";
+
+
